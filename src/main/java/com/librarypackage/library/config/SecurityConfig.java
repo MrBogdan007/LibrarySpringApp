710b6e6b29cf5c4b189928c75177d7292f626298
Bohdan <bogdanbrohovetsky@gmail.com>
2023-02-11 21:49:35 +0200

    added jwt security config, user, dependencies

---
+++ src/main/java/com/librarypackage/library/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.librarypackage.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	// generating default user
+	@Bean
+	public InMemoryUserDetailsManager user() {
+		return new InMemoryUserDetailsManager(
+				User.withUsername("mrbogdan").password("{noop}password").authorities("read").build());
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).authorizeRequests(auth -> auth
+				.anyRequest().authenticated())
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.httpBasic();
+		return http.build();
+	}
+}
