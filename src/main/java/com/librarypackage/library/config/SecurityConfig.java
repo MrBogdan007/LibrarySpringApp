f110d294e6a54a9fbce79173fb5c2fc416bb9373
MrBogdan007 <bogdabrohovetsky@gmail.com>
2023-02-13 17:21:19 +0200

    added JWT Encoder, generator

--- src/main/java/com/librarypackage/library/config/SecurityConfig.java
+++ src/main/java/com/librarypackage/library/config/SecurityConfig.java
@@ -8,17 +8,33 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	// generating default user
+
+	private final RsaKeyProperties rsaKeys;
 	@Bean
 	public InMemoryUserDetailsManager user() {
 		return new InMemoryUserDetailsManager(
 				User.withUsername("mrbogdan").password("{noop}password").authorities("read").build());
+	}
+
+	public SecurityConfig(RsaKeyProperties rsaKeys) {
+		super();
+		this.rsaKeys = rsaKeys;
 	}
 
 	@Bean
@@ -33,6 +49,12 @@ public class SecurityConfig {
 	}
 	@Bean
 	JwtDecoder jwtDecoder() {
-
+		return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+	}
+	@Bean
+	JwtEncoder jwtEncoder() {
+		JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
 	}
 }
