3c3e74280c7d5c994485efec7bda83921cf7b6da
Bohdan <bogdanbrohovetsky@gmail.com>
2023-02-14 20:10:43 +0200

    added method to extract email from JWT

---
+++ src/main/java/com/librarypackage/library/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.librarypackage.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.librarypackage.library.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+	private final UserRepository repository;
+@Bean
+public UserDetailsService userDetailsService() {
+		return username -> repository
+		.findByEmail(username)
+		.orElseThrow(() ->
+		new UsernameNotFoundException("User not found"));
+
+}
+}
--- src/main/java/com/librarypackage/library/config/JwtAuthenticationFilter.java
+++ src/main/java/com/librarypackage/library/config/JwtAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.librarypackage.library.config;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,7 +20,9 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	@NonNull
 	private final JwtService jwtService;
+	private final UserDetailsService userDetailsService;
 
 	@Override
 	protected void doFilterInternal(
@@ -35,6 +40,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		jwt = authHeader.substring(7);
 		//extract userEmail from JWT token;
 		userEmail = jwtService.extractUsername(jwt);
+		if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+		}
 	}
 
 }
